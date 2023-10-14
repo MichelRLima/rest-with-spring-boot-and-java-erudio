6747cf99a0632a0d5d8beb157cb3e5e895c2d4c0
Michel Rocha <michelrocha502@gmail.com>
2023-10-13 23:41:25 -0300

    Avançando com o desenvolvimento da API, implementando o banco de dados e ajusta das classes. Realizado validação da operação CRUD

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,79 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.exceptions.ResourceNotFoundExeception;
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+
+
+
+@Service
+public class PersonServices {
+	
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public List<Person> findAll() {
+		logger.info("Finding one persons");
+		
+		
+		
+		return repository.findAll();
+	}
+	
+	public Person findByIdD(Long id) {
+		
+		logger.info("Finding one person");
+		
+		
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundExeception("No records found for this ID"));
+	}
+	public Person create(Person person) {
+		
+		
+		logger.info("Creating one person");
+		
+		return repository.save(person);
+	}
+	
+	
+	public Person update(Person person) {
+		
+		
+		logger.info("Updating one person");
+		
+		var entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundExeception("No records found for this ID"));
+		
+	
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(entity);
+	}
+	public void delete(Long id) {
+		
+		logger.info("Deleting one person");
+		
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundExeception("No records found for this ID"));
+		
+		repository.delete(entity);
+		
+		
+	}
+	
+	
+
+}
